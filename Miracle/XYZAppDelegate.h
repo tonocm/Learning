272ea4e4c2db6270f8428da6cf36aeac09365577
tonocm <tonocm94@hotmail.com>
2014-05-22 18:40:14 -0400

    Initial Commit

---
+++ Miracle/XYZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  XYZAppDelegate.h
+//  Miracle
+//
+//  Created by hcardena on 5/22/14.
+//  Copyright (c) 2014 Miracles Can Happen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Miracle/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Miracle
+//
+//  Created by hcardena on 5/22/14.
+//  Copyright (c) 2014 Miracles Can Happen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "XYZAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([XYZAppDelegate class]));
+    }
+}
